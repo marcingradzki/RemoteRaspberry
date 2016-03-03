0fcf6aadac7451c8c788907fdbd7fad3cc09e28a
Lokiec1 <marcingradzk@gmail.com>
2016-03-03 14:34:53 +0100

    Added button sending message to log via POST method

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Remote GPIO' });
+});
+
+router.post('/', function(req, res){
+  console.log("Button has been pressed!");
 });
 
 module.exports = router;
