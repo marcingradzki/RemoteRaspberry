457350f6ea61343fb19532f1c07bea094fe55873
Lokiec1 <marcingradzk@gmail.com>
2016-03-05 20:14:57 +0100

    Added code to control GPIO port

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var gpio = require('rpi-gpio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Remote GPIO' });
 });
 
-router.post('/', function(req, res){
+var state = false;
+
+gpio.setup(13, gpio.DIR_OUT);
+
+router.post('/onoff', function(req, res){
   console.log("Button has been pressed!");
+  if(state){
+    gpio.write(13, state);
+    state = false;
+  }
+  else{
+    gpio.write(13, state);
+    state = true;
+  }
 });
 
 module.exports = router;
