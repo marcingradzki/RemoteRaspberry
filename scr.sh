d08b38d1acf7b0285c227b64d4d4cb51d92c7ee7
marcingradzki <marcingradzk@gmail.com>
2016-08-24 21:04:58 +0000

    Script for preparing raspbian to run this project

---
+++ scr.sh
@@ -0,0 +1,14 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install vim -y
+sudo apt-get install git-all -y
+cd /
+wget https://nodejs.org/dist/latest/node-v6.4.0-linux-armvl.tar.gz
+tar -xvf node-v6.4.0-linux-armv6l.tar.gz 
+cd node-v6.4.0-linux-armv6l
+node-v6.4.0-linux-armv6l
+sudo cp -R * /usr/local/
+cd ..
+rm node-v6.4.0-linux-armv61.tar.gz
+rm -rf node-v6.4.0-linux-armv61
+sudo reboot
