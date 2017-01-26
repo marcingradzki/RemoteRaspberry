55de8909e7b051fb13a6ea4b1a7d1ce35b55c9c5
marcingradzki <marcingradzk@gmail.com>
2017-01-26 22:34:08 +0000

    Privileges fix

--- scr.sh
+++ scr.sh
@@ -2,6 +2,8 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install vim -y
 sudo apt-get install git-all -y
+sudo apt-get install screen -y
+sudo apt-get install mongodb-server -y
 cd /
 wget https://nodejs.org/dist/latest/node-v6.4.0-linux-armvl.tar.gz
 tar -xvf node-v6.4.0-linux-armv6l.tar.gz 
